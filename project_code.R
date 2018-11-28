7f0fc96bd54b87d277b9828dd058733905667f3c
hannahpu <hannahpu@gmail.com>
2018-11-28 14:06:58 -0600

    Add models

--- project_code.R
+++ project_code.R
@@ -1,11 +1,21 @@
+#### Packages
+library(dplyr)
+library(randomForest)
+library(MASS)
+library(bigmemory)
+library(data.table)
+library(bigalgebra)
+library(irlba)
+library(glmnet)
+
 #### Data input
-pulocation = "/Users/hannahpu/OneDrive - University of Illinois - Urbana/STAT432/Project/"
-load(paste0(pulocation, "train_data.RData"))
-load(paste0(pulocation, "test_data.RData"))
+load("train_data.RData")
+load("test_data.RData")
 train.x = train_data
 test.x = test_data
-train.y = read.csv(paste0(pulocation, "train_labels.csv"), header = TRUE)
-test.y = read.csv(paste0(pulocation, "test_labels.csv"), header = TRUE)
+train.y = read.csv("train_labels.csv", header = TRUE)
+test.y = read.csv("test_labels.csv", header = TRUE)
+train = cbind(volcanoe = train.y[,1], train_data)
 
 #### Check dimension
 #train image file (no header) : 7000*12100
@@ -33,6 +43,69 @@ volplot = function(a){
   image(m, col = grey((0:255)/255))
 }
 volplot(train.x1[1,])
+volplot(train.x1[10,])
+volplot(train.x1[16,])
+
+####Preprocess
+# Dimension reduction using SVD.
+preprocess.svd = function(train, n.comp){
+  train[is.na(train)] = 0
+  z = svd(train[, 2:ncol(train)], nu=n.comp, nv=n.comp)
+  s = diag(z$d[1:n.comp])
+  train[, 2:ncol(train)] = z$u %*% s %*% t(z$v)
+  train
+}
+n.comp = 12
+preprocess.svd(train, n.comp)
+
+#####Model
+
+#Lasso
+#current result: accuracy 92.7944 (lambda= 0.001882239)
+train.x = as.matrix(train.x)
+train.y=as.matrix(train.y[,1])
+lasmodel = glmnet(x=train.x, y=train.y, alpha = 1, family = "binomial")
+
+pred = predict(lasmodel, newx=as.matrix(test.x), type = "response")
+predfunc= function(a){
+  ((sum(as.numeric(a > 0.5) == test.y[,1]))/ length(test.y[,1]))*100
+}
+pred.cal = apply(pred, 2, predfunc)
+pred.cal[which.max(pred.cal)+1]
+table(as.numeric(pred[,99] > 0.5), test.y[,1])
+
+#Lasso - with tuning
+#accuracy: 92.94% (lambda 0.001573354)
+train.x = as.matrix(train.x)
+train.y=as.matrix(train.y[,1])
+cv.out = cv.glmnet(x=train.x, y=train.y, alpha = 1, family = "binomial")
+lam.seq = exp(seq(-6.5, -5, length=100))
+lasmodel = glmnet(x=train.x, y=train.y, alpha = 1, family = "binomial", lambda = lam.seq)
+pred = predict(lasmodel, newx=as.matrix(test.x), type = "response")
+predfunc= function(a){
+  ((sum(as.numeric(a > 0.5) == test.y[,1]))/ length(test.y[,1]))*100
+}
+pred.cal = apply(pred, 2, predfunc)
+pred.cal[which.max(pred.cal)]
+table(as.numeric(pred[,97] > 0.5), test.y[,1])
+#Lasso selection-linear regression
+mylasso.coef.min = predict(lasmodel, s=cv.out$lambda.min, type="coefficients")
+mylasso.coef.1se = predict(lasmodel, s=cv.out$lambda.1se, type="coefficients")
+var.sel = row.names(mylasso.coef.1se)[nonzeroCoef(mylasso.coef.1se)[-1]]
+tmp.X = X[, colnames(X) %in% var.sel]
+
+
+#logistic
+logmodel = glm(volcanoe~., data=train.svd, family = binomial)
+
+#LDA
+ldmodel = lda(train.x, train.y)
+
+#random forest
+rfmodel = randomForest(train.x, y=train.y[,1])
+
+#neural network
+
 
 
 
